aa5f860b1d635c2ffd5c2dfee015dc37ff9e9254
Harsh Jain <hvj.techture@gmail.com>
2022-01-26 17:27:22 +0530

    intial commit with redux setup

---
+++ src/redux/userReducer.js
@@ -0,0 +1,15 @@
+
+const initalState = {
+    users:[],
+    user:{},
+    loading:false
+}
+
+
+const userReducer = (state =initalState , action) => {
+    switch(action.type){
+        default: return state
+    }
+}
+
+export default userReducer;
